0abca630c40ca7a6dedacc9777095cff555606b1
LordFeexz <anandafiqri1403@gmail.com>
2023-01-07 08:55:42 +0700

    test3

--- logictest/test3.js
+++ logictest/test3.js
@@ -21,37 +21,83 @@ OUTPUT:
 }
 
 */
+const counter = (food, price, money) => {
+  for (let i = 0; i < price.length; i++) {
+    for (let j = 0; j < food.length; j++) {
+      if (price[i].nama === food[j]) money -= price[i].harga;
+    }
+  }
+  return money;
+};
+
+const split = (str, operator) => {
+  let res = [];
+  let temp = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === operator || i === str.length - 1) {
+      if (i === str.length - 1) temp += str[i];
+      res.push(temp);
+      temp = "";
+    } else temp += str[i];
+  }
+  return res;
+};
 
 function jumlahTabungan(listHarga, history) {
-    // Write your code here
+  let obj = {};
+  let total = 0;
+
+  const splitRes = split(history, ".");
+  let day = [];
+  for (let i = 0; i < splitRes.length; i++) {
+    let temp = split(splitRes[i], "-");
+    day.push(temp);
+  }
+
+  for (let i = 0; i < day.length; i++) {
+    let food = split(day[i][1], ",");
+    const money = counter(food, listHarga, 10000);
+    obj[day[i][0]] = money;
+  }
+
+  let res = Object.values(obj);
+
+  for (let i = 0; i < res.length; i++) {
+    total += res[i];
+  }
+
+  obj["TotalTabungan"] = total;
+
+  return obj;
 }
 
 var hargaMakanan = [
   {
     nama: "ayam",
-    harga: 5000
+    harga: 5000,
   },
   {
     nama: "nasi",
-    harga: 2000
+    harga: 2000,
   },
   {
     nama: "cola",
-    harga: 1000
+    harga: 1000,
   },
   {
     nama: "chiki",
-    harga: 1500
+    harga: 1500,
   },
   {
     nama: "hotdog",
-    harga: 3000
+    harga: 3000,
   },
   {
     nama: "aqua",
-    harga: 2000
-  }
-]
+    harga: 2000,
+  },
+];
 
-var historyPembelian = `Senin-ayam,nasi,cola.Selasa-chiki,hotdog.Rabu-ayam,chiki.Kamis-hotdog.Jumat-chiki,cola,nasi`
-console.log(jumlahTabungan(hargaMakanan, historyPembelian))
+var historyPembelian = `Senin-ayam,nasi,cola.Selasa-chiki,hotdog.Rabu-ayam,chiki.Kamis-hotdog.Jumat-chiki,cola,nasi`;
+console.log(jumlahTabungan(hargaMakanan, historyPembelian));
